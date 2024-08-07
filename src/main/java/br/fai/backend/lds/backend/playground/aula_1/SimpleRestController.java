0d60a50f7f82e38d957410ec59b8f959e3982a9a
MichelecFernandes <michelecristinafr@gmail.com>
2024-08-06 22:24:19 -0300

    feat: manipulacao da api de playground

--- src/main/java/br/fai/backend/lds/backend/playground/aula_1/SimpleRestController.java
+++ src/main/java/br/fai/backend/lds/backend/playground/aula_1/SimpleRestController.java
@@ -1,6 +1,12 @@
 package br.fai.backend.lds.backend.playground.aula_1;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 //@RestController serve para deixar a classe exposta
 @RestController
@@ -8,25 +14,40 @@ public class SimpleRestController {
 
     //@GetMapping serve para deixar o metodo expostp
     @GetMapping("listar")
-    public String get(){
-        return "Boa noite";
+    public ResponseEntity<UserPlayground1> get(){
+        UserPlayground1 user = new UserPlayground1();
+        user.setId(10);
+        user.setEmail("user01@email.com");
+        user.setName("first name");
+
+        return ResponseEntity.ok().body(user);
     }
 
     @PostMapping("enviar")
-    public String post(){
-        return "Dados enviados";
+    public ResponseEntity<String> post(@RequestBody UserPlayground1 user){
+        System.out.println("id:" + user.getId());
+        System.out.println("email:" + user.getEmail());
+        System.out.println("nome:" + user.getName());
+        final URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(user.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
 
     }
 
-    @PutMapping("atualizar")
-    public String put(){
-        return "Dados atualizados";
+    @PutMapping("atualizar/{id}")
+    public ResponseEntity<String> put(@PathVariable int id, @RequestBody UserPlayground1 user){
+        System.out.println("id vindo da url: " + id);
+        System.out.println("id para atualizar:" + user.getId());
+        System.out.println("email para atualizar:" + user.getEmail());
+        System.out.println("nome para atualizar:" + user.getName());
+        return ResponseEntity.ok().body("Dados atualizados");
 
     }
 
-    @DeleteMapping("deletar")
-    public String delete(){
-        return "Dados deletados";
+    @DeleteMapping("deletar/{id}")
+    public ResponseEntity<String>  delete(@PathVariable int id){
+        System.out.println("id a ser removido:" + id);
+        return ResponseEntity.noContent().build();
 
     }
 
