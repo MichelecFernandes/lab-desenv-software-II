e7c2bc7d17041f52c869659b5582747c4afa6fef
MichelecFernandes <michelecristinafr@gmail.com>
2024-08-19 20:14:18 -0300

    feat: adicao dos models do projeto

---
+++ src/main/java/br/fai/backend/lds/backend/main/domain/Product.java
@@ -0,0 +1,15 @@
+package br.fai.backend.lds.backend.main.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private int id;
+    private String name;
+    private  double price;
+
+}
---
+++ src/main/java/br/fai/backend/lds/backend/main/domain/UserModel.java
@@ -0,0 +1,16 @@
+package br.fai.backend.lds.backend.main.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserModel {
+
+    private int id;
+    private String email;
+    private String password;
+    private String fullName;
+}
