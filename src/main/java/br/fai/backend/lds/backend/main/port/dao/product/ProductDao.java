45f77e663ab0f0773788e71ecd67b97c654f271d
MichelecFernandes <michelecristinafr@gmail.com>
2024-08-20 21:45:59 -0300

    feat: adicao das interfaces de Product

---
+++ src/main/java/br/fai/backend/lds/backend/main/port/dao/product/ProductDao.java
@@ -0,0 +1,8 @@
+package br.fai.backend.lds.backend.main.port.dao.product;
+
+
+import br.fai.backend.lds.backend.main.domain.Product;
+import br.fai.backend.lds.backend.main.port.dao.crud.CrudDao;
+
+public interface ProductDao extends CrudDao<Product> {
+}
---
+++ src/main/java/br/fai/backend/lds/backend/main/port/service/product/ProductService.java
@@ -0,0 +1,7 @@
+package br.fai.backend.lds.backend.main.port.service.product;
+
+import br.fai.backend.lds.backend.main.domain.Product;
+import br.fai.backend.lds.backend.main.port.service.crud.CrudService;
+
+public interface ProductService extends CrudService<Product> {
+}
