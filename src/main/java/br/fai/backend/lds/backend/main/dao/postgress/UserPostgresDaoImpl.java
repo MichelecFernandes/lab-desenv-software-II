031da1ef818974d065972a17de039abfa1b9e5a5
MichelecFernandes <michelecristinafr@gmail.com>
2024-09-16 20:23:14 -0300

    feat: finalizacao d implementacao de UserDao com Postgres

--- src/main/java/br/fai/backend/lds/backend/main/dao/postgress/UserPostgresDaoImpl.java
+++ src/main/java/br/fai/backend/lds/backend/main/dao/postgress/UserPostgresDaoImpl.java
@@ -7,7 +7,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class UserPostgresDaoImpl implements UserDao {
@@ -78,31 +80,157 @@ public class UserPostgresDaoImpl implements UserDao {
 
     @Override
     public void remove(int id) {
+        logger.log(Level.INFO, "Preparadando para remover a entidade com id " + id);
+        final String sql = "DELETE FROM user_model WHERE id = ?;";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            preparedStatement.execute();
+            preparedStatement.close();
+            logger.log(Level.INFO, "Entidade removida com sucesso");
+
+
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
 
     }
 
     @Override
     public UserModel readyById(int id) {
-        return null;
+        final String sql = "SELECT * FROM user_model WHERE id = ?;";
+
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                final UserModel user = new UserModel();
+                user.setId(resultSet.getInt("id"));
+                user.setFullName(resultSet.getString("fullName"));
+                user.setEmail(resultSet.getString("email"));
+                user.setPassword(resultSet.getString("password"));
+                logger.log(Level.INFO, "Entidade com id " + id + "encontrada com sucesso");
+
+                return user;
+
+            }
+
+            return null;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                resultSet.close();
+                preparedStatement.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
     }
 
     @Override
     public List<UserModel> readAll() {
-        return null;
+
+        final List<UserModel> users = new ArrayList<>();
+        final String sql = "SELECT * FROM user_model;";
+
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                final UserModel user = new UserModel();
+                user.setId(resultSet.getInt("id"));
+                user.setFullName(resultSet.getString("fullName"));
+                user.setEmail(resultSet.getString("email"));
+                user.setPassword(resultSet.getString("password"));
+                users.add(user);
+
+            }
+            resultSet.close();
+            preparedStatement.close();
+            return null;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void updateInformation(int id, UserModel entity) {
+        String sql = "UPDATE user_model SET fullName = ? WHERE id = ?;";
+
+        try {
+            PreparedStatement preparedStatement;
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, entity.getFullName());
+            preparedStatement.setInt(2, entity.getId());
+            preparedStatement.execute();
+            preparedStatement.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
     public UserModel readByEmail(String email) {
-        return null;
+        final String sql = "SELECT * FROM user_model WHERE email = ?;";
+
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, email);
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                final UserModel user = new UserModel();
+                user.setId(resultSet.getInt("id"));
+                user.setFullName(resultSet.getString("fullName"));
+                user.setPassword(resultSet.getString("password"));
+                logger.log(Level.INFO, "Entidade com email " + email + " encontrada com sucesso");
+
+                return user;
+
+            }
+
+            return null;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                resultSet.close();
+                preparedStatement.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
     }
+
 
     @Override
     public boolean updatePassword(int id, String newPassword) {
-        return false;
+        String sql = "UPDATE user_model SET password = ? WHERE id = ?;";
+
+
+        try {
+            PreparedStatement preparedStatement;
+            preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, newPassword);
+            preparedStatement.setInt(2, id);
+            preparedStatement.execute();
+            preparedStatement.close();
+            return true;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
