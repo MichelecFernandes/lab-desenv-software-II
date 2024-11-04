f96321da0bb3fd3adba11e57d792029a749113e2
MichelecFernandes <michelecristinafr@gmail.com>
2024-11-04 20:11:11 -0300

    prova: adicao das configuracoes do frame(quando uma tela chama a outra, por exemplo, uma propaganda de vendas na tela do g1) + h2-console

--- src/main/java/br/fai/backend/lds/backend/main/security/SecurityConfig.java
+++ src/main/java/br/fai/backend/lds/backend/main/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package br.fai.backend.lds.backend.main.security;
 
+import io.swagger.v3.oas.models.headers.Header;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -40,12 +42,18 @@ public class SecurityConfig {
                             "/listar",
                             "/swagger-ui/index.html",
                             "/swagger-ui/**",
-                            "/v3/api-docs/**"
+                            "/v3/api-docs/**",
+                            "/h2-console/**"
                     ).permitAll()
                      .anyRequest().authenticated()
             )
             .sessionManagement(
                     session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .headers(
+                    headers -> headers.frameOptions(
+                            HeadersConfigurer.FrameOptionsConfig::sameOrigin
+                    )
             );
 
 
