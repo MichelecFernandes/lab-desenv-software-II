e054092bd144308f4f40d890de0aed735dfb2da7
MichelecFernandes <michelecristinafr@gmail.com>
2024-11-05 22:50:48 -0300

    feat: implementacao incompleta da classe JwtServiceImpl

--- src/main/java/br/fai/backend/lds/backend/main/security/JwtServiceImpl.java
+++ src/main/java/br/fai/backend/lds/backend/main/security/JwtServiceImpl.java
@@ -2,50 +2,77 @@ package br.fai.backend.lds.backend.main.security;
 
 import br.fai.backend.lds.backend.main.domain.UserModel;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
 public class JwtServiceImpl implements JwtService{
+
+    private final String secret = "XUFAE3FQG1RLBlgQ93fDSUlj4HfbKi4a1kFl1gDloOg=";
     @Override
     public String getEmailFromToken(String token) {
-        return null;
+        return getClaimFromToken(token, Claims::getSubject);
     }
 
     @Override
     public Date getExpirationFromToken(String token) {
-        return null;
+        return getClaimFromToken(token, Claims::getExpiration);
     }
 
     @Override
     public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
-        return null;
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
     }
 
     @Override
     public Claims getAllClaimsFromToken(String token) {
-        return null;
+        return Jwts
+                .parserBuilder()
+                .setSigningKey("help")
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 
     @Override
     public boolean isTokenExpired(String token) {
-        return false;
+        final Date expirationDate = getExpirationFromToken(token);
+        return expirationDate.before(new Date());
     }
 
     @Override
     public boolean isTokenValid(String token, UserDetails userDetails) {
-        return false;
+        final String email = getEmailFromToken(token);
+        return email.equals(userDetails.getUsername()) && !isTokenExpired(token);
     }
 
     @Override
     public String generateToken(UserDetails userDetails, String fullName, UserModel.UserRole role, String email) {
-        return null;
+        final Map<String, Object> claims = new HashMap<>();
+        claims.put("email", email);
+        claims.put("fullName", fullName);
+        claims.put("role", role);
+        return createToken(claims, userDetails.getUsername());
     }
 
     @Override
     public String createToken(Map<String, Object> claims, String subject) {
-        return null;
+
+        final Date issueAt = new Date(System.currentTimeMillis());
+        final Date expirationDate = new Date(System.currentTimeMillis() + 100 * 60 * 60 * 10);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(issueAt)
+                .setExpiration(expirationDate)
+                .signWith(SignatureAlgorithm.HS256,secret)
+                .compact();
     }
 }
