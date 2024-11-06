4f8f991ba1e444dfbde643e098a4e8225fb8c40e
MichelecFernandes <michelecristinafr@gmail.com>
2024-11-05 21:51:38 -0300

    feat: criacao da interface JwtService a qual possui varios metodos que mostra o que a classe concreta(são classes derivadas de uma classe abstrata) que ira implementar esta interface ira fazer com seu uso

--- src/main/java/br/fai/backend/lds/backend/main/security/JwtService.java
+++ src/main/java/br/fai/backend/lds/backend/main/security/JwtService.java
@@ -1,4 +1,34 @@
 package br.fai.backend.lds.backend.main.security;
 
-public class JwtService {
+import br.fai.backend.lds.backend.main.domain.UserModel;
+import io.jsonwebtoken.Claims;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+public interface JwtService {
+
+    String getEmailFromToken(String token);
+
+    Date getExpirationFromToken(String token);
+
+    //<T> é o Generics
+    //Claims vem de um a dependencia de terceiros
+    <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver);
+
+    Claims getAllClaimsFromToken(String token);
+
+    boolean isTokenExpired(String token);
+
+    boolean isTokenValid(String token, UserDetails userDetails);
+
+    ///Esse metodo que eu injeto no token todas as informações do usuario
+    String generateToken(final UserDetails userDetails,
+                         final String fullName,
+                         final UserModel.UserRole role,
+                         final String email);
+
+    String createToken(Map<String, Object> claims, String subject);
 }
