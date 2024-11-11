b598e9d40a19cb3820fef93e60792e5636c40330
MichelecFernandes <michelecristinafr@gmail.com>
2024-11-11 20:42:51 -0300

    feat: adicao da geracao do token em AuthenticationController (sera removido daqui depois). Ou seja, uma vez que a request bate, a autenticacao eh realizada e por fim o token eh gerado

--- src/main/java/br/fai/backend/lds/backend/main/security/AuthenticationController.java
+++ src/main/java/br/fai/backend/lds/backend/main/security/AuthenticationController.java
@@ -8,6 +8,9 @@ import org.apache.coyote.Response;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +21,14 @@ public class AuthenticationController {
 
     private  final AuthenticationService authenticationService;
 
-    public AuthenticationController(AuthenticationService authenticationService) {
+    private final JwtService jwtService;
+
+    private final UserDetailsService userDetailsService;
+
+    public AuthenticationController(AuthenticationService authenticationService, JwtService jwtService, UserDetailsService userDetailsService) {
         this.authenticationService = authenticationService;
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
     }
 
     @PostMapping("/authenticate")
@@ -32,6 +41,24 @@ public class AuthenticationController {
         if(authenticatedUser == null){
             throw new BadCredentialsException("Email ou senha invalidos");
         }
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+        if(userDetails == null){
+            throw new UsernameNotFoundException("Email nao encontrado");
+        }
+
+        final String jwtToken = jwtService.generateToken(
+                userDetails,
+                authenticatedUser.getFullName(),
+                authenticatedUser.getRole(),
+                authenticatedUser.getEmail());
+
+        if(jwtToken == null || jwtToken.isEmpty()){
+            throw new InternalError("Jwt invalido");
+        }
+        System.out.println("Jwt criado:" + jwtToken);
+
         return ResponseEntity.ok(authenticatedUser);
+
     }
 }
